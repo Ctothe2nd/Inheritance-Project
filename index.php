e6dff022a48653d1541fd43b307a19cbfde0c2d7
ctothe2nd <christycasey25@gmail.com>
2019-03-24 17:52:40 -0400

    Inititial Commit
    This is the first basic iteration of the landing page

---
+++ index.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+
+<body>
+	<div class="timeline">
+	<div class="title">
+		<h1>The Inheritance Project</h1>
+		<h2>What is your inheritance?</h2>
+	</div>
+		<div class="row">
+		<!--<div class="left-tick">
+		<hr>
+		</div>-->
+		<div class="align-left">
+			<p>What is it that makes you...you?</p>
+		</div>
+		</div>
+		<div class="row"> 
+		<!--<div class="right-tick">
+		<hr>
+		</div>-->
+		<div class="align-right">
+			<h3>The Inheritance Project helps you understand how your inheritance creates who you are.</h3> 
+		</div>
+	</div>
+		<div class="row"> 
+		<!--<div class="right-tick">
+		<hr>
+		</div>-->
+		<div class="align-center outline">
+			<p class="black">This summer, the Inheritance Project will launch a storytelling platform for you to explore your inheritance, share your story and hear the stories of others.</p>
+		</div>
+		</div>
+		<div class="row">
+		<!--<div class="left-tick">
+		<hr>
+		</div>-->
+		<div class="align-left">
+			<p>Through a guided self-inquiry, you will begin </br> to discover how your unique inheritance—</p>
+		</div>
+		</div>
+		<div class="row"> 
+		<!--<div class="right-tick">
+		<hr>
+		</div>-->
+		<div class="align-center">
+			<p class="black"> your history, culture, traditions, beliefs, </br> social conditions, behaviors and experiences </p>
+		</div>
+		</div>
+		<div class="row"> 
+				<!--<div class="right-tick">
+				<hr>
+				</div>-->
+				<div class="align-right">
+					<p>—influences your identity. </p>
+				</div>
+			</div>
+		<div class="row"> 
+		<!--<div class="right-tick">
+		<hr>
+		</div>-->
+		<div class="align-center">
+			<h2>You are history in the making.</h2>  
+		</div>
+		</div>
+		<div class="row"> 
+		<!--<div class="right-tick">
+		<hr>
+		</div>-->
+		<div class="align-center outline">
+			<p>Join us May 24 - June 2, for the launch of  “Inheritance”, an immersive installation where founder Katya Stepanov takes visitors on a tour through a museum of her inherited identity as a Belarusian-Jewish immigrant, and explores how her cultural identity transformed coming of age in America. </br> </br>
+
+			This installation is supported by the COJECO BluePrint Fellowship.</p>  
+		</div>
+		</div>
+		<div class="row align-center"> 
+		<!-- Begin Mailchimp Signup Form -->
+			<!--<style type="text/css">
+				#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+				/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+				   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+			</style>-->
+			<div id="mc_embed_signup">
+			<form action="https://inheritanceproject.us20.list-manage.com/subscribe/post?u=43564a7ee2fcccd909e67ad3d&amp;id=ad7a85093e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				<div id="mc_embed_signup_scroll">
+				<label class="black label" for="mce-EMAIL">To stay informed about EARLY RELEASE TICKETS for INHERITANCE, as well as updated on the launch of THE INHERITANCE PROJECT, join our newsletter:</label></br>
+				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+				<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_43564a7ee2fcccd909e67ad3d_ad7a85093e" tabindex="-1" value=""></div>
+				<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+				</div>
+			</form>
+			</div>
+		<!--End mc_embed_signup-->
+		</div>
+		<div class="row align-center social-links">
+			<p>Follow us: </p> </br>
+<a href="https://www.instagram.com/inheritanceproject/">Instagram</a><a href="https://www.facebook.com/theinheritanceprojectny/">Facebook</a>
+
+		</div>
+
+
+	
+</body>
+</html>
